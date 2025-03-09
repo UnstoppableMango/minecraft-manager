3420123869baa46f2a57631f423f1011835697e9
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-03-08 20:39:19 -0600

    Durdling

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
 import { serve } from "bun";
 import index from "./index.html";
+import { CoreV1Api, CustomObjectsApi, KubeConfig } from '@kubernetes/client-node';
 
 const server = serve({
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
+
+    "/api/test": async () => {
+      const kc = new KubeConfig();
+      kc.loadFromDefault();
+
+      // const k8s = kc.makeApiClient(CustomObjectsApi);
+      // const obj = await k8s.listNamespacedCustomObject({
+      //   namespace: 'default',
+      //   group: 'shulkermc.io',
+      //   plural: 'minecraftclusters',
+      //   version: 'v1alpha1',
+      // });
+      const k8s = kc.makeApiClient(CoreV1Api);
+      const obj = await k8s.listNamespacedPod({ namespace: 'default' });
+
+      return Response.json({
+        message: JSON.stringify(obj),
+      });
+    },
 
     "/api/hello": {
       async GET(req) {
