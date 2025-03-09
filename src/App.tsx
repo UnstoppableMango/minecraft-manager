a1a7676036fadd5fe84414de6c90a410cf0f7627
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-03-08 20:39:19 -0600

    Durdling

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ export function App(): JSX.Element {
   const [loading, setLoading] = useState(true);
   const [versions, setVersions] = useState<McVersionsNet>(emptyMcVersionsNet);
   const [error, setError] = useState<string>();
+  const [res, setRes] = useState<string>();
 
   useEffect(() => {
     if (loading && !versions.stable.length) {
@@ -15,6 +16,12 @@ export function App(): JSX.Element {
         .catch(setError)
     }
   }, []);
+
+  useEffect(() => {
+    if (!res) {
+      fetch('/api/test').then(x => x.json()).then(x => setRes(x.message));
+    }
+  });
 
   if (error) {
     return (
@@ -27,12 +34,15 @@ export function App(): JSX.Element {
       <div className="mx-auto w-1/2 h-1/3 p-2 rounded-md bg-green-700">
         <label htmlFor="versions" className='p-2'>Versions</label>
         <select name="versions" className='p-1 rounded-md bg-gray-800'>
-          {versions.stable.map(v => (
-            <option key={v.semver} value={v.semver} className='p-3'>
-              <span>{v.semver} {v.date.toLocaleDateString()}</span>
+          {versions.stable.map((v, i) => (
+            <option key={i} value={v.semver} className='p-3'>
+              {v.semver} {v.date.toLocaleDateString()}
             </option>
           ))}
         </select>
+      </div>
+      <div>
+        <span>{res}</span>
       </div>
     </div>
   );
