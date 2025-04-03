62a2155e5da84a20af694067bd881843920b37a4
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-04-03 12:26:20 -0500

    re-run tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,8 @@ import React, { type ReactElement } from 'react';
 
 export function App(): ReactElement {
 	return (
-		<div className="bg-gray-800 w-full h-svh text-white">
-			<span>Hello there friendo</span>
+		<div className="bg-gray-800 w-full h-svh text-red-50">
+			<span>Hello there friendo!</span>
 		</div>
 	);
 }
