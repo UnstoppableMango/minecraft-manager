a835c136fbd2ef3a3a5b5fe502d383a0f56cd855
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-04-05 19:47:02 -0500

    Ignore start.ts I guess

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import reactX from 'eslint-plugin-react-x'
 import reactDom from 'eslint-plugin-react-dom'
 
 export default tseslint.config(
-	{ ignores: ['dist', 'src/dev'] },
+	{ ignores: ['dist', 'src/dev', 'start.ts'] },
 	{
 		extends: [
 			js.configs.recommended,
